60ff3dca6aa74afac499ab3a2d1811d8e0bccd3d
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-08-05 09:45:42 +1200

    template for site

---
+++ L3_Site_Template/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Put Content Here">
+    <meta name="keywords" content="Put keywords here">
+    <meta name="author" content="Put your name here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>DB Name</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="theme/l3_data_style.css"> 
+        <link rel="stylesheet" href="theme/font-awesome.min.css">
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+    
+            <h1>DB Name</h1>
+        </div>    <!-- / banner -->
+
+        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+        <div class="box nav">
+            
+            <div class="linkwrapper">
+                <div class="commonsearches">
+                    <a href="#l">All</a> | 
+                    <a href="#">Recent</a> | 
+                    <a href="#">Random</a> 
+                </div>  <!-- / common searches -->
+            
+                <div class="topsearch">
+                    
+                    <!-- Quick Search -->           
+                    <form method="post" action="quick_search.php" enctype="multipart/form-data">
+
+                        <input class="search quicksearch" type="text" name="quick_search" size="40" value="" required placeholder="Quick Search..." />
+
+                        <input class="submit" type="submit" name="find_quick" value="&#xf002;" />
+
+                    </form>     <!-- / quick search -->
+                    
+                </div>  <!-- / top search -->
+                
+                <div class="topadmin">
+                    <a href="#">Log In</a>
+                    
+                </div>  <!-- / top admin -->
+                
+            </div>  <!--- / link wrapper -->
+            
+        </div>    <!-- / nav -->        
+        
+        <div class="box main">
+            <h2>Find a Quote...</h2>
+            
+            <p>
+                Please use the quick search above to find quotes. Try...
+            </p>
+            <ul>
+                <i>Typing part of an author's names</i>
+                <i>Typing in a subject such as 'love' or 'life'</i>
+            </ul>
+            
+            <p>
+                Jelly beans gingerbread jelly tootsie roll. Lemon drops bear claw I love tiramisu candy canes cake. Jelly-o apple pie powder tart I    love candy tiramisu. Topping liquorice cake sesame snaps brownie. Lemon drops jelly beans brownie danish I love fruitcake cotton candy danish. Dragée pastry halvah macaroon I love chocolate bar caramels liquorice. Halvah cupcake muffin fruitcake pastry I love bear claw I love jelly. Tart chocolate bar croissant danish jelly. Sugar plum powder cupcake muffin gingerbread I love sweet.
+            </p>
+            
+            <p>
+                Biscuit jelly I love jelly-o candy canes chupa chups topping lemon drops I love. Sesame snaps macaroon lemon drops carrot cake I love. Chocolate bar gingerbread chupa chups. Dessert chocolate pastry liquorice tootsie roll lemon drops sweet macaroon liquorice. I love muffin bonbon. Cupcake apple pie chocolate cake bonbon topping. Macaroon gummi bears dragée caramels donut chocolate.
+            </p>
+            
+        </div>    <!-- / main -->
+        
+
+        <div class="box footer">
+            CC stevensc9277 2022
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>        
