2bbac65d15d2d8b21dc2983a205178a3b021d9b8
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-08-11 10:34:56 +1200

    content for main page

---
+++ L3_Site_Template/content/home.php
@@ -0,0 +1,22 @@
+<h2>Find a Quote...</h2>
+            
+            <p>
+                Please use the quick search above to find quotes. Try...
+            </p>
+            <ul>
+                <li>Typing part of an author's names</li>
+                <li>Typing in a subject such as 'love' or 'life'</li>
+            </ul>
+            
+            <p>
+                You can also...
+            </p>
+            <ul>
+                <li>Browse through <u>all the quotes</u></li>
+                <li>Grab <u>ten random quotes</u></li>
+                <li>See quotes that have <u>recently been added</u> to this database</li>
+            </ul>
+            <p>
+                <i>For each quote, you can click on the tags underneath it to find related quotes. If you click on the author's name, all the quotes by that author will be displayed</i>
+            </p>
+            
