f9cb67c35951ce7b98754d9d8f6aec7d7ff78dc9
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-08-11 10:34:45 +1200

    content for navigation bar

---
+++ L3_Site_Template/content/heading_navigation.php
@@ -0,0 +1,31 @@
+<!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+<div class="box nav">
+    
+    <div class="linkwrapper">
+        <div class="commonsearches">
+            <a href="#l">All</a> | 
+            <a href="#">Recent</a> | 
+            <a href="#">Random</a> 
+        </div>  <!-- / common searches -->
+    
+        <div class="topsearch">
+            
+            <!-- Quick Search -->           
+            <form method="post" action="quick_search.php" enctype="multipart/form-data">
+
+                <input class="search quicksearch" type="text" name="quick_search" size="40" value="" required placeholder="Quick Search..." />
+
+                <input class="submit" type="submit" name="find_quick" value="&#xf002;" />
+
+            </form>     <!-- / quick search -->
+            
+        </div>  <!-- / top search -->
+        
+        <div class="topadmin">
+            <a href="#">Log In</a>
+            
+        </div>  <!-- / top admin -->
+        
+    </div>  <!--- / link wrapper -->
+    
+</div>    <!-- / nav -->        
