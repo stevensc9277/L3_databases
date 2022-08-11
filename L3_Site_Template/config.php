8e7f6cb186cff503e764c077c702523da7a5647f
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2022-08-12 10:04:18 +1200

    changed db connect stuff

--- L3_Site_Template/config.php
+++ L3_Site_Template/config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define ("DB_NAME","editme"); 
-define ("DB_USERNAME","editme");
-define ("DB_PASSWORD","editme"); 
+define ("DB_NAME","stevensc9277_L3_Quotes"); 
+define ("DB_USERNAME","_stevensc9277");
+define ("DB_PASSWORD","y3F99iZfLXIRBPDo"); 
 define ("DB_HOST","localhost");
 
 ?>
